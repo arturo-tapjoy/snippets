dd18dfc5b647bbf514622295e25bfd2058642600
arturo-tapjoy <arturo.luna@tapjoy.com>
2017-02-24 09:12:46 -0800

    Update dashboard-DYNAMIC-VARIABLES.js
    adjusted google.com

--- dashboard-DYNAMIC-VARIABLES.js
+++ dashboard-DYNAMIC-VARIABLES.js
@@ -1,4 +1,4 @@
 currencyAmount: TAPJOY_CURRENCY_AMOUNT
 currencyName: TAPJOY_CURRENCY_NAME 
 iOSDestinationURL: http://www.apple.com
-androidDestinationURL: http://www.goggle.com
+androidDestinationURL: http://www.google.com
